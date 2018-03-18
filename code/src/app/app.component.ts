37eb4f08291df6e83544fabefe95abe1e3ce8e9f
pyp <pyp@github.com>
2018-03-18 17:18:18 -0300

    fix

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { GlobalService } from '../shared/services/global.service';
 import { LoginPage } from '../pages/login/login';
+import { ApplicationService } from '../shared/services/application.service';
 
 @Component({
     templateUrl: 'app.html'
@@ -20,7 +21,8 @@ export class EmotionsApp implements OnInit, OnDestroy {
         private platform: Platform,
         private statusBar: StatusBar,
         private splashScreen: SplashScreen,
-        private globalSrv:GlobalService,
+        private globalSrv: GlobalService,
+        private appSrv: ApplicationService
     ) {
         console.log('EmotionsApp constructor');
         platform.ready().then(() => {
@@ -34,9 +36,24 @@ export class EmotionsApp implements OnInit, OnDestroy {
     }
     ngOnInit(): void {
         console.log('EmotionsApp init');
-        this.globalSrv.get('user').subscribe(x=>{
+        this.globalSrv.get('user').subscribe(x => {
             if (x != null)
-              this.navCtrl.push('UsuarioPage', {});
+                this.navCtrl.push('UsuarioPage', {});
+        });
+        this.globalSrv.get('pushData').subscribe(x => {
+            if (x != null)
+                this.evalNotification(x);
         })
+    }
+
+    private evalNotification(data) {
+        if (data.type == "registro") {
+            this.appSrv.message('Se ha registrado la llave ' + data.llave);
+            this.navCtrl.push('UsuarioPage', data);
+        }
+        if (data.type == "devolucion") {
+            this.appSrv.message('Se ha devuelto la llave ' + data.llave);
+            this.navCtrl.push('UsuarioPage', data);
+        }
     }
 }
--- code/src/shared/services/pushing.service.ts
+++ code/src/shared/services/pushing.service.ts
@@ -1,62 +1,53 @@
 import { Injectable } from '@angular/core';
-import { Platform, NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { ApplicationService } from './application.service';
+import { GlobalService } from './global.service';
 
 declare const FCMPlugin: any;
 
-
 @Injectable()
 export class PushingService {
-    constructor(
-        private platform: Platform,
-        private appSrv: ApplicationService,
-        private navCtrl: NavController){
-        console.log('PushingService constructor');
+  constructor(
+    private platform: Platform,
+    private appSrv: ApplicationService,
+    private globalSrv: GlobalService
+  ) {
+    console.log('PushingService constructor');
+  }
+
+  initFCM(usr) {
+    if (((this.platform.is('mobileweb') == true) || (this.platform.is('core') == true)) == false) {
+      var self = this;
+      FCMPlugin.getToken(
+        function (id) {
+          console.log(id);
+          //self.registerUser(self.user, id);
+        },
+        function (err) {
+          console.log('error retrieving token: ' + err);
+        }
+      );
+
+      FCMPlugin.subscribeToTopic(usr);
+      //FCMPlugin.subscribeToTopic('registrationTopic');
+      //FCMPlugin.unsubscribeFromTopic('topicExample');
+
+      // FCMPlugin.onTokenRefresh().subscribe(id=>{
+      //   alert('token refresh!');
+      //   this.registerUser(this.user, id);
+      // })
+
+      FCMPlugin.onNotification(
+        function (data) {
+          this.globalSrv.set('pushData', data);
+        },
+        function (msg) {
+          console.log('onNotification callback successfully registered: ' + msg);
+        },
+        function (err) {
+          console.log('Error registering onNotification callback: ' + err);
+        }
+      );
     }
-    
-    initFCM(usr) {
-        if (((this.platform.is('mobileweb') == true) || (this.platform.is('core') == true)) == false) {
-          var self = this;
-          FCMPlugin.getToken(
-            function (id) {
-              console.log(id);
-              //self.registerUser(self.user, id);
-            },
-            function (err) {
-              console.log('error retrieving token: ' + err);
-            }
-          );
-    
-          FCMPlugin.subscribeToTopic(usr);
-          //FCMPlugin.subscribeToTopic('registrationTopic');
-          //FCMPlugin.unsubscribeFromTopic('topicExample');
-    
-          // FCMPlugin.onTokenRefresh().subscribe(id=>{
-          //   alert('token refresh!');
-          //   this.registerUser(this.user, id);
-          // })
-    
-          FCMPlugin.onNotification(
-            function (data) {
-              self.evalNotification(data);
-            },
-            function (msg) {
-              console.log('onNotification callback successfully registered: ' + msg);
-            },
-            function (err) {
-              console.log('Error registering onNotification callback: ' + err);
-            }
-          );
-        }
-      }
-      private evalNotification(data) {
-        if (data.type == "registro") {
-          this.appSrv.message('Se ha registrado la llave '+ data.llave);
-          this.navCtrl.push('UsuarioPage', data);
-        }
-        if (data.type == "devolucion") {
-          this.appSrv.message('Se ha devuelto la llave '+ data.llave);
-          this.navCtrl.push('UsuarioPage', data);
-        }
-      }
+  }
 }
