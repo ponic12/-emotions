715537bb66f408bdf31bc6000d3b4aec40ae717e
pyp <pyp@github.com>
2018-07-19 22:23:38 -0300

    1st code push

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { SplashScreen } from '@ionic-native/splash-screen'
 import { StatusBar } from '@ionic-native/status-bar'
+import { CodePush } from '@ionic-native/code-push'
+
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular'
 import { EmotionsApp } from './app.component'
 import { SharedModule } from '../shared/shared.module'
@@ -30,6 +32,7 @@ import { FIREBASE_CONFIG } from '../shared/services/firebase.config'
       EmotionsApp
    ],
    providers: [
+      CodePush,
       FirebaseService,
       StatusBar,
       SplashScreen,
