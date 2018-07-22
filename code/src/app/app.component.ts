16b83fd36f51d7d607b04ff232f24fe925e2df65
pyp <pyp@github.com>
2018-07-21 22:25:24 -0300

    fix statusbar

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -87,7 +87,7 @@ export class EmotionsApp implements OnInit, OnDestroy {
 
          // Okay, so the platform is ready and our plugins are available.
          // Here you can do any higher level native things you might need.
-         this.statusBar.styleDefault()
+         this.statusBar.styleLightContent()
          //statusBar.styleBlackOpaque()
          this.splashScreen.hide()
       }).catch(err => {
